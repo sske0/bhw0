32868d2be2a81b44f03056af915f4d9de932a6aa
sske0 <akbaraz@code.edu.az>
2023-05-24 17:54:38 +0400

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bhw1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Ali";
+            ViewBag.Surname = "Aliyev";
+            ViewBag.Age = 25;
+            ViewBag.Country = "Azerbaijan";
+            ViewBag.PhoneNumber = "+994551234567";
+
+            return View();
+        }
+    }
+}
